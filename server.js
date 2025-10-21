559bda946eb62785c5e44413c85ab17a40e3be6e
abumahmud <alabiabdulbaasit@gmail.com>
2025-10-21 15:02:01 +0100

    tried to make mocha work

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 
@@ -9,14 +9,16 @@ app.use(helmet());
 app.use(cors());
 
 // Test homepage
-app.get('/', (req, res) => {
-  res.send('Converter is running');
+app.get("/", (req, res) => {
+  res.send("Converter is running");
 });
 
 // Route setup
-require('./routes/api.js')(app);
+require("./routes/api.js")(app);
 
 // 404 (wrong address)
-app.use((req, res) => res.status(404).send('Not Found'));
+app.use((req, res) => res.status(404).send("Not Found"));
 
-app.listen(3000, () => console.log('Running on port 3000'));
+app.listen(3000, () => console.log("Running on port 3000"));
+
+module.exports = app;
